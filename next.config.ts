899c78ee085d9a3f9fbcd9f53a4d266ae109a97b
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-05-06 16:21:47 +0100

    👕 feat: update next.config.ts to ignore build and lint errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  ignoreBuildErrors: true,
+  // also ignore lint errors
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+    
 };
 
 export default nextConfig;
